3e498debf71f9e4c47e5a600d318854f0a0ec780
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-08-30 15:59:09 +0530

    Stored Computed Value in Dictionary

---
+++ ArithmeticOperation6.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+read -p "Enter the First Input:" input1
+read -p "Enter the Second Input:" input2
+read -p "Enter the Third Input:" input3
+
+a=$input1
+b=$input2
+c=$input3
+
+result1=$(( a + b * c ))
+       echo "$result1"
+result2=$(( a * b + c))
+        echo "$result2"
+result3=`awk 'BEGIN { printf("%2d\n", ('$c' + '$a') / '$b' )}'`
+        echo "$result3"
+result4=`awk 'BEGIN { printf("%2d\n", '$a' % ('$b' + '$c' ) )}'`
+        echo "$result4"
+declare -A result
+        result[computation1]=$result1
+        result[computation2]=$result2
+        result[computation3]=$result3
+        result[computation4]=$result4
+echo ${result[@]}
